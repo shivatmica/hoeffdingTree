be3a5637304b4eb92603194b7a151b155e66a035
motiwari <mohittiwarinyc+github@gmail.com>
2022-08-01 16:55:16 -0700

    Cleanup

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 from skmultiflow.data import SEAGenerator
 from skmultiflow.trees import HoeffdingTreeClassifier
 from skmultiflow.data import RegressionGenerator
@@ -27,11 +26,9 @@ def init(
             raise Exception(f"{label} not found in the DataFrame.")
         X = main_df.drop(label, axis=1)
         y = main_df[label]
-
         X_train, y_train, X_test, y_test = train_test_split(
             X, y, train_size=train_size, test_size=test_size, random_state=101
         )
-
     else:
         train_df = pd.read_csv(train)
         test_df = pd.read_csv(test)
@@ -39,34 +36,32 @@ def init(
             raise Exception(f"{label} not found in the train dataset.")
         if label not in test_df.columns:
             raise Exception(f"{label} not found in the test dataset.")
-
         X_train = train_df.drop(label, axis=1)
         y_train = train_df[label]
         X_test = test_df.drop(label, axis=1)
         y_test = test_df[label]
-
     return (X_train, y_train, X_test, y_test)
 
 
 def hoeffding(
-    X_train, y_train, max_samples, n_samples, classification=False, regression=False
+    max_samples,
+    classification=False,
+    regression=False,
 ):
-    if classification == False and regression == False:
+    if classification is False and regression is False:
         raise Exception(
             "No mode selected, please set classification or regression to true."
         )
-    if classification == True and regression == True:
+    elif classification is True and regression is True:
         raise Exception("Both parameters are True, please set only one as True.")
 
     if classification:
         stream = SEAGenerator(random_state=1)
         ht = HoeffdingTreeClassifier(leaf_prediction="nb")
-
         curr_samples = 0
         tp = 0
         fp = 0
         max_samples = max_samples
-
         while curr_samples < max_samples and stream.has_more_samples():
             X_test, y_test = stream.next_sample()
             y_pred = ht.predict(X_test)
@@ -76,24 +71,18 @@ def hoeffding(
                 fp += 1
             ht = ht.partial_fit(X_test, y_test)
             curr_samples += 1
-
         return {tp / curr_samples}
-
-    if regression:
+    elif regression:
         stream = RegressionGenerator(random_state=1, n_samples=10000)
-
         ht_reg = HoeffdingTreeRegressor()
-
         n_samples_cnt = 0
         max_samples = 100000
         y_pred = np.zeros(max_samples)
         y_true = np.zeros(max_samples)
-
         while n_samples_cnt < max_samples and stream.has_more_samples():
             X_train, y_train = stream.next_sample()
             y_true[n_samples_cnt] = y_train[0]
             y_pred[n_samples_cnt] = ht_reg.predict(X_train)[0]
             ht_reg.partial_fit(X_train, y_train)
             n_samples_cnt += 1
-
         return np.mean(np.abs(y_true - y_pred))
