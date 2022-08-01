0067a5c4f26ca6bd18a969985892ad1eee926b5b
shivatmica <62459136+shivatmica@users.noreply.github.com>
2022-08-01 14:16:16 +0530

    Testing the utils

---
+++ test.py
@@ -0,0 +1,3 @@
+from utils import *
+X_train, y_train, X_test, y_test = init(label = 'PM2.5', train = 'C:\\Users\\Asus\\Downloads\\PRSA_Data_Dingling_20130301-20170228.csv', split = True)
+print(hoeffding(X_test, y_test, regression = True, max_samples = 100000, n_samples = 10000))
