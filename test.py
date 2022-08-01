51f21c63c4c4f7978bcd0f3109c73139bd3856df
motiwari <mohittiwarinyc+github@gmail.com>
2022-08-01 16:39:40 -0700

    Running black

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 from utils import *
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # regression
-    X_train, y_train, X_test, y_test = init(label = 'PM2.5', train = 'C:\\Users\\Asus\\Downloads\\PRSA_Data_Dingling_20130301-20170228.csv', split = True)
-    print(hoeffding(X_test, y_test, regression = True, max_samples = 100000, n_samples = 10000))
+    X_train, y_train, X_test, y_test = init(
+        label="PM2.5",
+        train="C:\\Users\\Asus\\Downloads\\PRSA_Data_Dingling_20130301-20170228.csv",
+        split=True,
+    )
+    print(
+        hoeffding(X_test, y_test, regression=True, max_samples=100000, n_samples=10000)
+    )
--- utils.py
+++ utils.py
@@ -7,40 +7,57 @@ from skmultiflow.data import RegressionGenerator
 from skmultiflow.trees import HoeffdingTreeRegressor
 from sklearn.model_selection import train_test_split
 
-def init(label, test_size = 0.2, train_size = 0.8, train = 'aps_failure_training_set_processed_8bit.csv', test = 'aps_failure_test_set_processed_8bit.csv', split = False):
+
+def init(
+    label,
+    test_size=0.2,
+    train_size=0.8,
+    train="aps_failure_training_set_processed_8bit.csv",
+    test="aps_failure_test_set_processed_8bit.csv",
+    split=False,
+):
     if test_size + train_size != 1.0:
-        raise Exception(f'test_size and train_size must add up to 1.0, but got {train_size + test_size} instead.')
+        raise Exception(
+            f"test_size and train_size must add up to 1.0, but got {train_size + test_size} instead."
+        )
 
     if split:
         main_df = pd.read_csv(train)
         if label not in main_df.columns:
-            raise Exception(f'{label} not found in the DataFrame.')
-        X = main_df.drop(label, axis = 1)
+            raise Exception(f"{label} not found in the DataFrame.")
+        X = main_df.drop(label, axis=1)
         y = main_df[label]
 
-        X_train, y_train, X_test, y_test = train_test_split(X, y, train_size = train_size, test_size = test_size, random_state = 101)
+        X_train, y_train, X_test, y_test = train_test_split(
+            X, y, train_size=train_size, test_size=test_size, random_state=101
+        )
 
     else:
         train_df = pd.read_csv(train)
         test_df = pd.read_csv(test)
         if label not in train_df.columns:
-            raise Exception(f'{label} not found in the train dataset.')
+            raise Exception(f"{label} not found in the train dataset.")
         if label not in test_df.columns:
-            raise Exception(f'{label} not found in the test dataset.')
-        
-        X_train = train_df.drop(label, axis = 1)
+            raise Exception(f"{label} not found in the test dataset.")
+
+        X_train = train_df.drop(label, axis=1)
         y_train = train_df[label]
-        X_test = test_df.drop(label, axis = 1) 
+        X_test = test_df.drop(label, axis=1)
         y_test = test_df[label]
 
     return (X_train, y_train, X_test, y_test)
 
-def hoeffding(X_train, y_train, max_samples, n_samples, classification = False, regression = False):
+
+def hoeffding(
+    X_train, y_train, max_samples, n_samples, classification=False, regression=False
+):
     if classification == False and regression == False:
-        raise Exception('No mode selected, please set classification or regression to true.')
+        raise Exception(
+            "No mode selected, please set classification or regression to true."
+        )
     if classification == True and regression == True:
-        raise Exception('Both parameters are True, please set only one as True.')
-    
+        raise Exception("Both parameters are True, please set only one as True.")
+
     if classification:
         stream = SEAGenerator(random_state=1)
         ht = HoeffdingTreeClassifier(leaf_prediction="nb")
@@ -59,7 +76,7 @@ def hoeffding(X_train, y_train, max_samples, n_samples, classification = False, 
                 fp += 1
             ht = ht.partial_fit(X_test, y_test)
             curr_samples += 1
-        
+
         return {tp / curr_samples}
 
     if regression:
@@ -79,4 +96,4 @@ def hoeffding(X_train, y_train, max_samples, n_samples, classification = False, 
             ht_reg.partial_fit(X_train, y_train)
             n_samples_cnt += 1
 
-        return (np.mean(np.abs(y_true - y_pred)))
+        return np.mean(np.abs(y_true - y_pred))
